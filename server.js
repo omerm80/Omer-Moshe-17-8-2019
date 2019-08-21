d34d64445e0a7d8138fd268812e62de7b4ff606e
omerm80 <omerm80@gmail.com>
2019-08-21 21:56:33 +0300

    test

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const express=require('express');
+const app=express();
+const path=require('path');
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT||8000);
+app.get('/*',function(req,res)
+{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+})
+console.log('Console listening!');
